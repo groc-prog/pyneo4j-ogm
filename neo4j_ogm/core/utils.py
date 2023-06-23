3d5d526e1e17cca6d971673a7a7daba0a27cb8c2
groc-prog <marc.troisner@gmail.com>
2023-06-23 17:58:44 +0200

    feat(utils): Merge ensure_alive and ensure_hydrated decorators to one

--- neo4j_ogm/core/utils.py
+++ neo4j_ogm/core/utils.py
@@ -7,27 +7,9 @@ from typing import Callable
 from neo4j_ogm.core.exceptions import InstanceDestroyed, InstanceNotHydrated
 
 
-def ensure_hydration(func: Callable):
-    """
-    Decorator which ensures that a instance has been hydrated before running any queries.
-
-    Raises:
-        InstanceNotHydrated: Raised if the method is called on a instance which has not been hydrated
-    """
-
-    async def decorator(self, *args, **kwargs):
-        if getattr(self, "_element_id", None) is None:
-            raise InstanceNotHydrated()
-
-        result = await func(self, *args, **kwargs)
-        return result
-
-    return decorator
-
-
 def ensure_alive(func: Callable):
     """
-    Decorator which ensures that a instance has not been destroyed before running any queries.
+    Decorator which ensures that a instance has not been destroyed and has been hydrated before running any queries.
 
     Raises:
         InstanceDestroyed: Raised if the method is called on a instance which has been destroyed
@@ -36,6 +18,9 @@ def ensure_alive(func: Callable):
     async def decorator(self, *args, **kwargs):
         if getattr(self, "_destroyed", True):
             raise InstanceDestroyed()
+
+        if getattr(self, "_element_id", None) is None:
+            raise InstanceNotHydrated()
 
         result = await func(self, *args, **kwargs)
         return result
