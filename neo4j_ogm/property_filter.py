e87e224ddd0699f7fa37cc988b7b1d648bbc09bc
groc-prog <marc.troisner@gmail.com>
2023-06-27 22:03:05 +0200

    feat(filters): Combined filter options $and, $or, $xor

--- neo4j_ogm/property_filter.py
+++ neo4j_ogm/property_filter.py
@@ -18,14 +18,15 @@ BASIC_OPERANTS: dict[str, Any] = {
     "$regex": "=~",
 }
 
-COMBINED_OPERANTS: list[str] = [
-    "$and",
-    "$or",
-    "$xor",
-]
+COMBINED_OPERANTS: dict[str, Any] = {
+    "$and": "AND",
+    "$or": "OR",
+    "$xor": "XOR",
+}
 
 
 class QueryBuilder:
+    property_name: str = ""
     ref: str = ""
     parameters: dict[str, Any] = {}
     queries: list[str] = []
@@ -42,8 +43,22 @@ class QueryBuilder:
                 parsed_filters[property_name] = {"$eq": operants_or_value}
 
         for property_name, operants in parsed_filters.items():
+            self.property_name = property_name
+
             for operant_name, operant_value in operants.items():
-                query, parameter = self._build_operant(property_name, operant_name, operant_value)
+                query = ""
+                parameter = {}
+
+                if operant_name in BASIC_OPERANTS:
+                    query, parameter = self._build_basic_operant(operant_name, operant_value)
+                elif operant_name == "$exists":
+                    query = self._build_exists_operant(operant_value)
+                elif operant_name == "$not":
+                    query, parameter = self._build_not_operant(operant_value)
+                elif operant_name in COMBINED_OPERANTS:
+                    query, parameter = self._build_combined_operant(operant_name, operant_value)
+                else:
+                    query, parameter = self._build_basic_operant("$eq", operant_value)
 
                 self.queries.append(query)
                 self.parameters = {**self.parameters, **parameter}
@@ -51,64 +66,65 @@ class QueryBuilder:
         query = f"WHERE {' AND '.join(self.queries)}"
         return query, self.parameters
 
-    def _build_operant(
-        self, property_name: str, operant_name: str, operant_value: Any, param_prefix: str | None = None
-    ):
-        parameter: dict[str, Any] = {}
-        query = ""
+    def _build_basic_operant(
+        self, operant_name: str, operant_value: Any, param_prefix: str | None = None
+    ) -> tuple[str, dict[str, Any]]:
+        parameter = {}
 
-        if operant_name in BASIC_OPERANTS:
-            parameter_name = (
-                f"{property_name}_{operant_name[1:]}"
-                if param_prefix is None
-                else f"{property_name}_{param_prefix}_{operant_name[1:]}"
-            )
-            query = f"{self.ref}.{property_name} {BASIC_OPERANTS[operant_name]} ${parameter_name}"
-            parameter[parameter_name] = operant_value
-        elif operant_name == "$not":
-            not_operant_name, not_operator_value = next(iter(cast(dict, operant_value).items()))
-            partial_query, param = self._build_operant(property_name, not_operant_name, not_operator_value, "not")
+        parameter_name = (
+            f"{self.property_name}_{operant_name[1:]}"
+            if param_prefix is None
+            else f"{self.property_name}_{param_prefix}_{operant_name[1:]}"
+        )
 
-            query = f"NOT ({partial_query})"
-            parameter = param
-        elif operant_name == "$exists":
-            if operant_value:
-                query = f"{self.ref}.{property_name} IS NOT NULL"
-            else:
-                query = f"{self.ref}.{property_name} IS NULL"
-        else:
-            parameter_name = f"{property_name}_eq" if param_prefix is None else f"{property_name}_{param_prefix}_eq"
-            query = f"{self.ref}.{property_name} {BASIC_OPERANTS['$eq']} ${parameter_name}"
-            parameter[parameter_name] = operant_value
+        query = f"{self.ref}.{self.property_name} {BASIC_OPERANTS[operant_name]} ${parameter_name}"
+        parameter[parameter_name] = operant_value
 
         return query, parameter
 
-        # for operant_name, operant_value in operants.items():
-        #     # Handle basic operants
-        #     if operant_name in BASIC_OPERANTS:
-        #         parameter_name = (
-        #             f"{property_name}_{operant_name[1:]}"
-        #             if param_prefix is None
-        #             else f"{property_name}_{param_prefix}_{operant_name[1:]}"
-        #         )
-        #         query = f"{self.ref}.{property_name} {BASIC_OPERANTS[operant_name]} ${parameter_name}"
+    def _build_exists_operant(self, exists: bool) -> str:
+        return f"{self.ref}.{self.property_name} IS NOT NULL" if exists else f"{self.ref}.{self.property_name} IS NULL"
 
-        #         self.queries.append(query)
-        #         self.parameters[parameter_name] = operant_value
-        #     # Handle operants which combine other operants
-        #     elif operant_name in COMBINED_OPERANTS:
-        #         pass
-        #     # Handle special operant `$exists` for property existence checks
-        #     elif operant_name == "$not":
-        #         self.
-        #     elif operant_name == "$exists":
-        #         query = (
-        #             f"{self.ref}.{property_name} IS NOT NULL"
-        #             if operant_value
-        #             else f"{self.ref}.{property_name} IS NULL"
-        #         )
+    def _build_not_operant(self, operant_value: Any, param_prefix: str | None = None) -> tuple[str, dict[str, Any]]:
+        # Only first key-value pair is used
+        not_operant_name, not_operator_value = next(iter(cast(dict, operant_value).items()))  #
 
-        #         self.queries.append(query)
+        prefix = f"{param_prefix}_not" if param_prefix is not None else "not"
+        basic_operant_query, basic_operant_parameter = self._build_basic_operant(
+            not_operant_name, not_operator_value, param_prefix=prefix
+        )
+
+        query = f"NOT ({basic_operant_query})"
+        return query, basic_operant_parameter
+
+    def _build_combined_operant(
+        self, operant_type: str, operants: list[dict[str, Any]], param_prefix: str | None = None
+    ) -> tuple[str, dict[str, Any]]:
+        prefix = f"{param_prefix}_{operant_type[1:]}" if param_prefix is not None else operant_type[1:]
+        queries = []
+        parameters = {}
+
+        for operant in operants:
+            for operant_name, operant_value in operant.items():
+                query = ""
+                parameter = {}
+
+                if operant_name in BASIC_OPERANTS:
+                    query, parameter = self._build_basic_operant(operant_name, operant_value, prefix)
+                elif operant_name == "$exists":
+                    query = self._build_exists_operant(operant_value)
+                elif operant_name == "$not":
+                    query, parameter = self._build_not_operant(operant_value, prefix)
+                elif operant_name in COMBINED_OPERANTS:
+                    query, parameter = self._build_combined_operant(operant_name, operant_value, prefix)
+                else:
+                    query, parameter = self._build_basic_operant("$eq", operant_value, prefix)
+
+                queries.append(query)
+                parameters = {**parameters, **parameter}
+
+        query = f"({f' {COMBINED_OPERANTS[operant_type]} '.join(queries)})"
+        return query, parameters
 
 
 test_filter = {
@@ -118,13 +134,13 @@ test_filter = {
     "prop4": 12,
     "prop5": {"$exists": True},
     "prop6": {"$not": {"$lte": 20}},
-    # "prop7": {"$and": [{"$starts_with": "foo"}, {"$ends_with": "bar"}]},
+    "prop7": {"$or": [{"$contains": "abc"}, {"$and": [{"$starts_with": "foo"}, {"$ends_with": "bar"}]}]},
+    # WHERE (n.prop7 CONTAINS $prop7_or_contains OR (n.prop7 STARTS WITH $prop7_or_and_starts_with AND n.prop7 ENDS WITH $prop7_or_and_ends_with))
 }
 
 builder = QueryBuilder()
 complete_query, params = builder.build_operant_queries("n", test_filter)
 print("DONE")
-
 
 # WHERE (n.prop4 < 40 AND (n.prop4 > 20 OR n.prop4 = 18))
 # WHERE n.prop1 > $prop1_gt AND n.prop2 IN $prop2_in AND n.prop3 STARTS WITH $prop3_starts_with AND (n.prop4 > $prop4_and_gt AND n.prop4 < $prop4_and_lt)
@@ -160,3 +176,42 @@ print("DONE")
 #         "$regex": "stajfoafe",  # REGEX
 #     },
 # }
+
+# def _build_operant(
+#     self, property_name: str, operant_name: str, operant_value: Any, param_prefix: str | None = None
+# ):
+#     parameter: dict[str, Any] = {}
+#     query = ""
+
+#     # Basic operants which don't need any kind of special treatment
+#     if operant_name in BASIC_OPERANTS:
+#         parameter_name = (
+#             f"{property_name}_{operant_name[1:]}"
+#             if param_prefix is None
+#             else f"{property_name}_{param_prefix}_{operant_name[1:]}"
+#         )
+#         query = f"{self.ref}.{property_name} {BASIC_OPERANTS[operant_name]} ${parameter_name}"
+#         parameter[parameter_name] = operant_value
+#     # Handle operants which combine other basic operants
+#     elif operant_name in COMBINED_OPERANTS:
+
+#     # Handle special `$not` operant separately
+#     elif operant_name == "$not":
+#         # Only first key-value pair is used
+#         not_operant_name, not_operator_value = next(iter(cast(dict, operant_value).items()))
+#         partial_query, param = self._build_operant(property_name, not_operant_name, not_operator_value, "not")
+
+#         query = f"NOT ({partial_query})"
+#         parameter = param
+#     # Handle special `$exists` operant which does not require a parameter
+#     elif operant_name == "$exists":
+#         if operant_value:
+#             query = f"{self.ref}.{property_name} IS NOT NULL"
+#         else:
+#             query = f"{self.ref}.{property_name} IS NULL"
+#     else:
+#         parameter_name = f"{property_name}_eq" if param_prefix is None else f"{property_name}_{param_prefix}_eq"
+#         query = f"{self.ref}.{property_name} {BASIC_OPERANTS['$eq']} ${parameter_name}"
+#         parameter[parameter_name] = operant_value
+
+#     return query, parameter
