1fc776a2da9f442d0c2d620d8820f79ea81cff21
groc-prog <marc.troisner@gmail.com>
2023-07-07 16:58:03 +0200

    feat: Update to different index types

--- neo4j_ogm/fields.py
+++ neo4j_ogm/fields.py
@@ -7,15 +7,23 @@ from typing import Type, TypeVar
 T = TypeVar("T")
 
 
-def WithOptions(property_type: T, indexed: bool = False, unique: bool = False, ref: str | None = None) -> Type[T]:
+def WithOptions(
+    property_type: T,
+    range_index: bool = False,
+    text_index: bool = False,
+    point_index: bool = False,
+    unique: bool = False,
+    ref: str | None = None,
+) -> Type[T]:
     """
-    Returns a subclass of `property_type` which includes extra attributes like `_indexed` and `_unique`
-    which can be used to define indexes and constraints on the property. Does not have an effect when called
-    with just the `property_type` argument.
+    Returns a subclass of `property_type` which includes extra attributes which can be used to define indexes and
+    constraints on the property. Does not have an effect when called with just the `property_type` argument.
 
     Args:
         property_type (Any): The property type to return for the model field
-        indexed (bool, optional): Whether the property should be indexed or not. Defaults to False.
+        range_index (bool, optional): Whether the property should have a `RANGE` index or not. Defaults to False.
+        text_index (bool, optional): Whether the property should have a `TEXT` index or not. Defaults to False.
+        point_index (bool, optional): Whether the property should have a `POINT` index or not. Defaults to False.
         unique (bool, optional): Whether a `UNIQUENESS` constraint should be created for the property.
             Defaults to False.
 
@@ -28,7 +36,9 @@ def WithOptions(property_type: T, indexed: bool = False, unique: bool = False, r
         Subclass of provided type with extra arguments
         """
 
-        _indexed: bool = indexed
+        _range_index: bool = range_index
+        _text_index: bool = text_index
+        _point_index: bool = point_index
         _unique: bool = unique
         _ref: str | None = ref
 
