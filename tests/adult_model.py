0ef30e9b1a946407206260c1b2f9e148ea36d27a
groc-prog <marc.troisner@gmail.com>
2023-07-25 20:09:59 +0200

    refactor: Move test files

--- tests/adult_model.py
+++ tests/adult_model.py
@@ -10,12 +10,19 @@ from neo4j_ogm.fields.relationship_property import RelationshipProperty
 from neo4j_ogm.queries.types import RelationshipDirection
 
 
-class AdultToAdult(RelationshipModel):
-    since: datetime = Field(default_factory=datetime.now)
-    best_friends: bool = False
+class Married(RelationshipModel):
+    wedding_day: datetime = Field(default_factory=datetime.now)
 
     class Settings:
-        type = "HAS_CHILD"
+        type = "IS_TOGETHER_WITH"
+
+
+class Friends(RelationshipModel):
+    since: datetime = Field(default_factory=datetime.now)
+    best_friends: bool
+
+    class Settings:
+        type = "IS_FRIENDS_WITH"
 
 
 class Adult(NodeModel):
@@ -24,7 +31,10 @@ class Adult(NodeModel):
     age: int
 
     friends = RelationshipProperty(
-        target_model="Adult", relationship_model=AdultToAdult, direction=RelationshipDirection.BOTH
+        target_model="Adult", relationship_model=Friends, direction=RelationshipDirection.BOTH, allow_multiple=True
+    )
+    partner = RelationshipProperty(
+        target_model="Adult", relationship_model=Married, direction=RelationshipDirection.BOTH
     )
     children = RelationshipProperty(
         target_model="Child", relationship_model="HasChild", direction=RelationshipDirection.OUTGOING
--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,23 @@
 # pylint: disable=missing-class-docstring missing-module-docstring
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 import asyncio
 import random
 from datetime import datetime
 
 from neo4j_ogm.core.client import Neo4jClient
-from tests.models.adult_model import Adult, AdultToAdult
-from tests.models.child_model import Child, HasChild
-from tests.models.special.toy_model import OwnsToy, Toy
+from tests.adult_model import Adult, Friends, Married
+from tests.child_model import Child, HasChild
+from tests.toy_model import OwnsToy, Toy
 
 client = Neo4jClient()
 client.connect(uri="neo4j://localhost:7687", auth=("neo4j", "password"))
 
 
 async def setup() -> None:
-    await client.register_models([Adult, AdultToAdult, Child, HasChild, Toy, OwnsToy])
+    await client.register_models([Adult, Married, Friends, Child, HasChild, Toy, OwnsToy])
     await client.drop_constraints()
     await client.drop_indexes()
     await client.drop_nodes()
@@ -28,12 +32,34 @@ async def setup() -> None:
     child1 = await Child(name="Jamie").create()
     child2 = await Child(name="Emma").create()
     child3 = await Child(name="Noah").create()
-    child4 = await Child(name="Olivia").create()
 
     toy1 = await Toy(make="ToysAreUs", produced_at=random.randint(0, int(datetime.timestamp(datetime.now())))).create()
     toy2 = await Toy(make="PlayTime", produced_at=random.randint(0, int(datetime.timestamp(datetime.now())))).create()
     toy3 = await Toy(make="FunFuntime", produced_at=random.randint(0, int(datetime.timestamp(datetime.now())))).create()
     toy4 = await Toy(make="KidsJoy", produced_at=random.randint(0, int(datetime.timestamp(datetime.now())))).create()
+
+    await adult1.partner.connect(adult2)
+    await adult5.partner.connect(adult6)
+
+    await adult1.friends.connect(adult3, {"since": datetime.now(), "best_friends": False})
+    await adult1.friends.connect(adult4, {"since": datetime.now(), "best_friends": True})
+    await adult2.friends.connect(adult4, {"since": datetime.now(), "best_friends": False})
+    await adult3.friends.connect(adult4, {"since": datetime.now(), "best_friends": True})
+    await adult5.friends.connect(adult4, {"since": datetime.now(), "best_friends": False})
+    await adult6.friends.connect(adult4, {"since": datetime.now(), "best_friends": False})
+
+    await adult1.children.connect(child1)
+    await adult2.children.connect(child1)
+    await adult5.children.connect(child2)
+    await adult5.children.connect(child3)
+    await adult6.children.connect(child2)
+    await adult6.children.connect(child3)
+
+    await child3.toys.connect(toy1)
+    await child3.toys.connect(toy2)
+    await child2.toys.connect(toy2)
+    await child2.toys.connect(toy3)
+    await child1.toys.connect(toy4)
 
 
 async def main():
