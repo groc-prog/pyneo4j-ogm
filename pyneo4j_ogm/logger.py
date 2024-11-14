c9d39b9c77f946253f69537a43628ff37f7dca27
groc-prog <marc.troisner@gmail.com>
2024-11-14 19:47:34 +0100

    feat(logger): use env variables for configuration

--- pyneo4j_ogm/logger.py
+++ pyneo4j_ogm/logger.py
@@ -1,5 +1,10 @@
 import logging
 
+from pyneo4j_ogm.env import EnvVariable, from_env
+
+enable_logging = int(from_env(EnvVariable.LOGGING_ENABLED, 1))
+log_level = int(from_env(EnvVariable.LOGLEVEL, logging.WARNING))
+
 logger = logging.getLogger("pyneo4j-ogm")
 logger.setLevel(logging.WARNING)
 
@@ -9,3 +14,6 @@ handler.setLevel(logging.WARNING)
 formatter = logging.Formatter("[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+if not bool(enable_logging):
+    logger.disabled = True
